7656c90bd4ce4fe82d5ee2682533fcc6ccba09eb
Nathan Maynard <nathanmaynard@MacBook-Pro.local>
2022-10-07 00:23:00 +0100

    Initialized Project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+namespace consoleCombat // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+            WriteLine("Hello World!");
+            WriteLine("Hello World!");
+            WriteLine("Hello World!");
+            WriteLine("Hello World!");
+        }
+    }
+}
